646121544ba682f31342e1834f93a30f0cc7bf5f
name <cybersteer@naver.com>
2022-03-27 09:31:02 +0900

    A/B

---
+++ InputAndOutput/src/Ex08.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+        System.out.println(a/b);
+    }
+}
