884f94a224ea11f2ac6868d2e13b40a16a39a593
name <cybersteer@naver.com>
2022-03-27 09:37:13 +0900

    ??!

---
+++ InputAndOutput/src/Ex10.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str+"??!");
+    }
+}
